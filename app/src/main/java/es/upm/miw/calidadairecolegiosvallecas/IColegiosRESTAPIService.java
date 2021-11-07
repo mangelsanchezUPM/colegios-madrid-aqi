8cabbf6191b302680ec39ed161af6e7f89217787
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-11-07 12:28:45 +0100

    Corregida URL de consulta Colegios

--- app/src/main/java/es/upm/miw/calidadairecolegiosvallecas/IColegiosRESTAPIService.java
+++ app/src/main/java/es/upm/miw/calidadairecolegiosvallecas/IColegiosRESTAPIService.java
@@ -5,6 +5,8 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 interface IColegiosRESTAPIService {
-    @GET("202311-0-colegios-publicos.json?latitud=40.4&longitud=-3.4&distancia=3000")
+    // URL devuelve todos los colegios de Madrid, los parametros de url se han puesto para evitar
+    // invalidaciones de JSON (id duplicado)
+    @GET("202311-0-colegios-publicos.json?latitud=40.47210978701385&longitud=-3.631007402473766&distancia=200000")
     Call<Colegios> getColegios();
 }
