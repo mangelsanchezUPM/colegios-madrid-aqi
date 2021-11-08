15fe1909e29e7a5939c5599c41a5d20dfc76e346
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-11-08 16:28:46 +0100

    Guardar registro en BD

--- app/src/main/java/es/upm/miw/colegiosmadridaqi/DetallesActivity.java
+++ app/src/main/java/es/upm/miw/colegiosmadridaqi/DetallesActivity.java
@@ -2,17 +2,22 @@ package es.upm.miw.colegiosmadridaqi;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import colegiosmadridaqi.R;
-import es.upm.miw.colegiosmadridaqi.models.Iaqi;
 
 public class DetallesActivity extends AppCompatActivity {
     final static String LOG_TAG = "MiW";
@@ -24,10 +29,10 @@ public class DetallesActivity extends AppCompatActivity {
     private Integer aqi;
     private String nombreColegio;
     private FirebaseUser user;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    private static final int RC_SIGN_IN = 2018;
+    private final String DB_URL = "https://colegios-madrid-aq-default-rtdb.europe-west1.firebasedatabase.app/";
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -55,11 +60,28 @@ public class DetallesActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.no2)).setText("NO2: " + extras.getDouble("no2"));
             ((TextView) findViewById(R.id.p)).setText("P: " + extras.getDouble("p"));
             ((TextView) findViewById(R.id.t)).setText("T: " + extras.getDouble("t"));
-            ((TextView) findViewById(R.id.pm10)).setText("PM10: " + extras.getDouble("pm10"));
-            ((TextView) findViewById(R.id.pm25)).setText("PM25: " + extras.getDouble("pm25"));
+            ((TextView) findViewById(R.id.pm10)).setText("PM10: " + extras.getInt("pm10"));
+            ((TextView) findViewById(R.id.pm25)).setText("PM25: " + extras.getInt("pm25"));
             ((TextView) findViewById(R.id.w)).setText("W: " + extras.getDouble("w"));
             ((TextView) findViewById(R.id.wg)).setText("WG: " + extras.getDouble("wg"));
 
+            database = FirebaseDatabase.getInstance(DB_URL);
+            myRef = database.getReference("aqi-colegios");
+
+            Button btnGuardar = findViewById(R.id.btnGuardar);
+            if (user == null) btnGuardar.setVisibility(View.INVISIBLE);
+            else {
+                btnGuardar.setOnClickListener(view -> {
+                    Map<String, Object> registro = new HashMap<>();
+                    registro.put("usuario", user.getDisplayName());
+                    registro.put("colegio", nombreColegio);
+                    registro.put("aqi", aqi);
+                    myRef.setValue(registro);
+                    Toast.makeText(this.getApplicationContext(), "Registro insertado", Toast.LENGTH_SHORT).show();
+                });
+            }
+        } else {
+            finish();
         }
     }
 }
